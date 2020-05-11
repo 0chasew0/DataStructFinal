2878f03bbcd684975607c943a3db5acce2fe3828
HongSLin <63325729+HongSLin@users.noreply.github.com>
2020-05-10 22:10:32 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,38 @@
-#include "BuildTree.h"
 #include "TreeNode.h"
-
-#include <iostream>
+#include "BuildTree.h"
 
 int main() {
+    // WWARNING Each time you run it, the expression.txt will only have the expression2
+    std::string expression1 = "(5*(6+7))", expression2 = "(8/(7-6))";
 
-	// Object which reads our expression from the text file and creates an empty root node
-	BuildTree b("expression.txt");
+    // Object which reads our expression from the text file and creates an empty root node
+    BuildTree b("expression.txt");
 
-	std::cout << "Expression result is: " << b.evaluate(b.build(b.tokenize()));
+    std::cout <<  b.evaluate(b.build(b.tokenize()));
+    std::cout << " is the result" << "\n";
 
-	return 0;
 
+
+    std::cout <<"\n\n";
+    std::ofstream output;
+    output.open("expression.txt");
+    output << expression1;
+    output.close();
+
+    BuildTree c("expression.txt");
+    std::cout <<  c.evaluate(c.build(c.tokenize()));
+    std::cout << " is the result" << "\n";
+
+
+
+    std::cout <<"\n\n";
+    output.open("expression.txt");
+    output << expression2;
+    output.close();
+
+    BuildTree d("expression.txt");
+    std::cout <<  d.evaluate(d.build(d.tokenize()));
+    std::cout << " is the result" << "\n";
+
+    return 0;
 }
